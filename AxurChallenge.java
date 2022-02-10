3eb4b84c9254b2656f530fddfb4f8fcb84e7bad8
rafael <rafaelfelipe501@gmail.com>
2022-02-10 20:18:07 -0300

    Versão Args

--- AxurChallenge.java
+++ AxurChallenge.java
@@ -6,7 +6,8 @@ import java.io.BufferedReader;
 public class AxurChallenge {
     public static void main(String[] args){
         try {
-            URL url = new URL("https://pastebin.com/m4bV1yic");
+            URL url = new URL("http://www.mocky.io/v2/5e18df272f00007e0097e1b4");
+            System.out.println(args[0]);
 
             BufferedReader bufferedPage = Page.getPage(url);
 
@@ -22,5 +23,4 @@ public class AxurChallenge {
         }
     };
 }
-// https://www.kabum.com.br/?gclid=Cj0KCQiAjJOQBhCkARIsAEKMtO0WjqpCpefe73Dy-GqM2mbrJ26tk47nwVT9AkmFQ4bUWu27SYPj-vMaAhyMEALw_wcB
 // http://www.mocky.io/v2/5e18df272f00007e0097e1b4
